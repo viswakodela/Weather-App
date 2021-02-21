7e1f4a9e48a9e253f0317645568a8a0c4274d70b
Viswa Kodela <viswakodela@gmail.com>
2021-02-21 17:21:00 -0500

    Initial Commit

---
+++ Weather App/Weather_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Weather_AppApp.swift
+//  Weather App
+//
+//  Created by Viswa Kodela on 2/21/21.
+//
+
+import SwiftUI
+
+@main
+struct Weather_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
